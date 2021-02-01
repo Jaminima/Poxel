91431831d96ad1ae4e729fae0366a9e92506e12a
Oscar Davies <oscar.william.davies@outlook.com>
2021-02-01 14:22:05 +0000

    [Broken] But has cool effect

--- Poxel/Poxel.h
+++ Poxel/Poxel.h
@@ -17,20 +17,27 @@ public:
 		enabled = false;
 	}
 
+	bool CheckAndSet(array_view<Poxel, 2> poxel, unsigned int x, unsigned int y) restrict(amp) {
+		if (!poxel[y][x].enabled) {
+			Poxel* p = &poxel[y][x];
+
+			p->enabled = true;
+			p->color = color;
+
+			enabled = false;
+			return true;
+		}
+		return false;
+	}
+
 	void doStep(array_view<Poxel, 2> poxel, array_view<Color, 3> rgbView, index<2> idx, bool bufferSwitch) restrict (amp) {
 		rgbView[bufferSwitch][idx] = poxel[idx].color;
 
 		if (idx[0] > 0) { //Prevent falling out of scene
 
-			if (!poxel[idx[0] - 1][idx[1]].enabled) {
-				Poxel* p = &poxel[idx[0] - 1][idx[1]];
-
-				p->enabled = true;
-				p->color = color;
-
-				enabled = false;
-			}
-
+			if (!CheckAndSet(poxel ,idx[0]-1,idx[1])) {}
+			else if (!CheckAndSet(poxel, idx[0] - 1, idx[1] - 1)) {}
+			else if (!CheckAndSet(poxel, idx[0] - 1, idx[1] + 1)) {}
 		}
 	}
 };
